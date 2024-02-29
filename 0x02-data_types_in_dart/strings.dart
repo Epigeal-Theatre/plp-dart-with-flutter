227ffc1d47140c5585e376072af73865d13d453a
Epigeal-Theatre <danielonyinkwa1@gmail.com>
2024-02-29 22:39:29 +0300

    strings in dart

---
+++ 0x02-data_types_in_dart/strings.dart
@@ -0,0 +1,14 @@
+void main() {
+  //strings in dart can be enclosed in either single or double quotes
+  String name = "Gallagher";
+  String occupation = "Business man!";
+
+  print("My name is $name and I am a $occupation!");
+
+  //remember that using var directly will still give the same output
+
+  var name2 = "Daniel";
+  var marital_status = "single";
+
+  print("My name is $name2 and I am $marital_status!");
+}

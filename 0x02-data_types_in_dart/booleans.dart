04ff3a597eeeb5f3a12ffd05587d363671de6d80
Epigeal-Theatre <danielonyinkwa1@gmail.com>
2024-02-29 22:53:49 +0300

    booleans

---
+++ 0x02-data_types_in_dart/booleans.dart
@@ -0,0 +1,14 @@
+//bools are initialized with the keyword bool
+//booleans are simply true/ false statements
+/*in dart, the bool values true and false are case
+sensitive and must start with a small letter.
+True is not the same as true and False is not
+the same as false*/
+
+void main() {
+  var is_married = false;
+  var loves_coding = true;
+
+  print(is_married);
+  print(loves_coding);
+}

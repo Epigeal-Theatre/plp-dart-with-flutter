de78189aba914799b69fda1bdd80f07ab539e890
Epigeal-Theatre <danielonyinkwa1@gmail.com>
2024-02-29 21:09:03 +0300

    comments in dart

---
+++ comments.dart
@@ -0,0 +1,20 @@
+/*comments in dart are a way to include
+text inside of our files that will not be
+executed by the compiler. comments are ignored by the compler*/
+
+//we have two types of comments.Multiline comments and single line comments.
+
+//this is a sinle line comment
+
+/*this is 
+a multiline
+comment in 
+dart programming language.*/
+
+void main() {
+  print("this is hello world1 without comment\n");
+  //print("this is hello world2 with singleline comments\n")
+  /*print("this is hello world3 with multiline comments\n")*/
+  print(
+      "hello world4.if you see only hello world 1 and 4, it is because 2 and 3 were ignored by the compiler because they are comments\n");
+}

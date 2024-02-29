eee123e3a8b6e730d15b677911cfc30af00c7807
Epigeal-Theatre <danielonyinkwa1@gmail.com>
2024-02-29 20:55:26 +0300

    add info about var variable type

--- variables.dart
+++ variables.dart
@@ -7,10 +7,23 @@ void main() {
   num sum = 20;
   bool is_married = true;
   String my_name = "Gallagher";
+  /*we also have a var type called var. 
+  it doesnt need to be told the type of
+  variable as it can think based
+  on what you input*/
+  var pet = "chop";
+  var fav_decimal = 10.8;
+  var phones = 2;
+  var do_you_love_coding = true;
 
   print(children);
   print(gpa);
   print(sum);
   print(is_married);
   print(my_name);
+
+  print(pet);
+  print(fav_decimal);
+  print(phones);
+  print(do_you_love_coding);
 }

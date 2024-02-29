d4ba197de2f44e190618910d6205840b973eceb1
Epigeal-Theatre <danielonyinkwa1@gmail.com>
2024-02-29 20:47:33 +0300

    variables in dart

---
+++ variables.dart
@@ -0,0 +1,16 @@
+/*variables in dart, just like in any other lang
+are used to store value*/
+//variables in dart include the following
+void main() {
+  int children = 10;
+  double gpa = 2.2;
+  num sum = 20;
+  bool is_married = true;
+  String my_name = "Gallagher";
+
+  print(children);
+  print(gpa);
+  print(sum);
+  print(is_married);
+  print(my_name);
+}

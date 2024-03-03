81e7400dbfa7b3df9582b3b40883a3c9cc2b7d7a
Epigeal-Theatre <danielonyinkwa1@gmail.com>
2024-03-03 09:33:41 +0300

    arithmetic operations

---
+++ 0x03-arithmetic_operations/arithmetic_operations.dart
@@ -0,0 +1,25 @@
+/**
+ * >htis is arithmetic operations in dart
+ * >
+ * >
+ */
+void main() {
+  //declaration of integer values//
+  int number1 = 5;
+  int number2 = 20;
+
+  //now lets demonstrate some basic operations//
+  int sum = number1 + number2;
+  int subt = number2 - number1;
+  int mul = number1 * number2;
+  int div = number2 ~/ number1;
+  double divi = number2 / number1;
+  int mod = number2 % number1;
+
+  print(sum);
+  print(subt);
+  print(mul);
+  print(div);
+  print(divi);
+  print(mod);
+}

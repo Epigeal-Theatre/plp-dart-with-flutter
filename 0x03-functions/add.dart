5aae00fba970a49def8712b93deed7118e400a4e
Epigeal-Theatre <danielonyinkwa1@gmail.com>
2024-03-04 23:35:31 +0300

    add

---
+++ 0x03-functions/add.dart
@@ -0,0 +1,15 @@
+/**
+ * htis is a simple function that will add sum of 2 ints
+ * it takes 2 parameters and ha s avoid return type
+ */
+
+//write the add function
+void add(int num1, int num2) {
+  int sum = num1 + num2;
+  print("The sum of the two numbers is :$sum");
+}
+
+//now lets write our main function and call add from within it
+void main() {
+  add(10, 20);
+}

e123326780f9cfb13a320eeed89f7384b10135b1
Epigeal-Theatre <danielonyinkwa1@gmail.com>
2024-03-07 16:39:34 +0300

    fruits

---
+++ 0x05-anonymous_functions/example1.dart
@@ -0,0 +1,8 @@
+void main() {
+  const fruits = ["apple", "banana", "cherry"];
+
+  fruits.forEach((fruit) {
+    print(fruit);
+  });
+}
+//this function invokes each fruit without having a fuction name

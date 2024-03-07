34e93cff70ea40b7eddc0d2eb356fabe2ae4cd7d
Epigeal-Theatre <danielonyinkwa1@gmail.com>
2024-03-07 18:10:20 +0300

    stringLength

---
+++ dart-functions_assignment/stringLength.dart
@@ -0,0 +1,9 @@
+/**
+ * Write a function called stringLength that takes an argument of type String and returns the length of that string.
+ */
+stringLength(String inputString) => inputString.length;
+
+void main() {
+  var len = stringLength("Hello Gallagher!");
+  print("Length of this string is : $len");
+}

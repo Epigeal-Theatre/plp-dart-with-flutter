4bdec7c8b22321a2fb0280a4b86ef0e5e92f7cd8
Epigeal-Theatre <danielonyinkwa1@gmail.com>
2024-03-07 17:53:20 +0300

    fivideTwo

---
+++ dart-functions_assignment/divideTwo.dart
@@ -0,0 +1,11 @@
+/**
+ * Task 4
+Write a function called divideTwo that takes two numbers as arguments and returns the quotient of those two numbers.
+ */
+
+divideTwo(num1, num2) => num1 ~/ num2;
+
+void main() {
+  var result = divideTwo(50, 25);
+  print("The answer to this division task is :$result");
+}

d318a296eaa634b0c07ad4d3bb566cf4d9aed17a
Epigeal-Theatre <danielonyinkwa1@gmail.com>
2024-03-07 17:41:05 +0300

    subtractTwo

---
+++ dart-functions_assignment/subtractTwo.dart
@@ -0,0 +1,11 @@
+/**
+ *Task 2
+Write a function called subtractTwo that takes two numbers as arguments and returns the difference of those two numbers
+ */
+
+subtractTwo(num1, num2) => num1 - num2;
+
+void main() {
+  var result = subtractTwo(100, 50);
+  print("The answer to this task is: $result");
+}

3dcfcd23b4a92b40c85bac41bd242f17d88011a3
Epigeal-Theatre <danielonyinkwa1@gmail.com>
2024-03-07 17:44:44 +0300

    multiplyTwo

---
+++ dart-functions_assignment/multiplyTwo.dart
@@ -0,0 +1,11 @@
+/**
+ * Task 3
+Write a function called multiplyTwo that takes two numbers as arguments and returns the product of those two numbers.
+ */
+
+multiplyTwo(num1, num2) => num1 * num2;
+
+void main() {
+  var result = multiplyTwo(4, 5);
+  print("And the answer to this task is :$result");
+}

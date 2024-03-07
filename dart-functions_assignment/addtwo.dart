a7f4194911d9152e431adb9ec76821f80338de13
Epigeal-Theatre <danielonyinkwa1@gmail.com>
2024-03-07 17:34:52 +0300

    addTwo corrections

--- dart-functions_assignment/addtwo.dart
+++ dart-functions_assignment/addtwo.dart
@@ -1,4 +1,9 @@
-//Task 1
-Write a function called addTwo that takes two numbers as arguments and returns the sum of those two numbers.
+/*Task 1
+Write a function called addTwo that takes two numbers as arguments and returns the sum of those two numbers.*/
 
+addTwo(num1, num2) => num1 + num2;
 
+void main() {
+  var result = addTwo(10, 20);
+  print("And the answer to this task is $result");
+}
